d73ea3c0278e4285d6a2192d6d315d895b29f3aa
SilfSanya <silfsanya@gmail.com>
2025-08-27 20:53:07 +0200

    js-hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,7 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
-    
-}
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
 
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+inputEl.addEventListener("input", () => {
+  const trimmedValue = inputEl.value.trim();
+  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
